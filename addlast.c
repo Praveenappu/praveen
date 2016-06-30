3649a0f7aca87a18e6a69c33865bd469adddf8be
Praveen kumar j <pravinair0@gmail.com>
2016-06-30 13:47:50 +0530

    Create addlast.c

---
+++ addlast.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,e,f;
+clrscr();
+printf("Enter the numbers:\n");
+scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+f=a+e;
+printf("the answer is:%d",f);
+getch();
+}
