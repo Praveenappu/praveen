34e7fd1b0138a11da4076b62c8210170c8ef7543
Praveen kumar j <pravinair0@gmail.com>
2016-06-30 11:34:46 +0530

    Create squareindividual.c

---
+++ squareindividual.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d;
+clrscr();
+printf("enter the integer:\n");
+scanf("%d%d%d",&a,&b,&c);
+d=a*a+b*b+c*c;
+printf("the output is:%d\n",d);
+getch();
+}
+
+
+
+
