7d24fd1226110180609a0eb8024c6b925c700ae7
Praveen kumar j <pravinair0@gmail.com>
2016-07-01 14:50:40 +0530

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+printf("Enter the number:\n");
+clrscr();
+scanf("%d",&i);
+if(i%2==0)
+{
+printf("No is even");
+}
+else
+{
+printf("No is odd");
+}
+getch();
+}
