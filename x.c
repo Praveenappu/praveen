16daf6bb2b4801550958e5f5f5c19098165e22db
Praveen kumar j <pravinair0@gmail.com>
2016-07-01 15:08:34 +0530

    Create x.c

---
+++ x.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+clrscr();
+printf("the shape is:\n");
+for(i=0;i<=0;i++)
+{
+printf("\t\t\tx\n");
+printf("\t\tx\t\tx\n");
+printf("\tx\t\tx\t\tx\n");
+printf("\t\tx\t\tx\n");
+printf("\t\t\tx\n");
+}
+getch();
+}
