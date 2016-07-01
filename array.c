a808be8c17fffc96c526f553a2a2ce991c5b2f85
Praveen kumar j <pravinair0@gmail.com>
2016-07-01 14:59:44 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],b[10],c,d,sum,ans,add;
+clrscr();
+printf("the array is :\n");
+scanf("%d%d",&a[c],&b[d]);
+if(a[c]!=b[d])
+{
+printf("invalid");
+}
+else
+{
+sum=a[c];
+add=b[d];
+ans=sum+add;
+printf("the no:%d",ans);
+}
+getch();
+}
