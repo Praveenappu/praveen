7186578b91a42bdc5274af89e58b117331107667
Praveen kumar j <pravinair0@gmail.com>
2016-07-01 15:04:07 +0530

    Create diamond.c

---
+++ diamond.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+clrscr();
+printf("the shape is:\n");
+for(i=0;i<=0;i++)
+{
+printf("\t\t\t*\n");
+printf("\t\t*\t\t*\n");
+printf("\t*\t\t*\t\t*\n");
+printf("\t\t*\t\t*\n");
+printf("\t\t\t*\n");
+}
+getch();
+}
