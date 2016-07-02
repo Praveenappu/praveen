34590e0c6e70992943b2c01e6c2cf5bcd22c3511
Praveen kumar j <pravinair0@gmail.com>
2016-07-02 14:29:06 +0530

    Create least.c

---
+++ least.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d;
+clrscr();
+printf("Enter the numbers:\n");
+scanf("%d%d%d%d",&a,&b,&c,&d)'
+if(a>b)
+{
+printf("%d",a);
+}
+else if(a>c)
+{
+printf("%d",a)
+}
+else if(b>c)
+{
+printf("%d",b)
+}
+else if(c>d)
+{
+printf("%d",c);
+}
+getch();
+}
+
+
