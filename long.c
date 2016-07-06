0867f845d038f3d1c5c2cbdd0aea76b556e316c5
Praveen kumar j <pravinair0@gmail.com>
2016-07-06 12:16:26 +0530

    Create long.c

---
+++ long.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a,b;
+printf("ENTER the string:");
+scanf("%c\n%c",&a,&b);
+if(a>'a '|| a<'z')
+{
+else if(a>b)
+{
+printf("%c is a largest string");
+}
+else if(a>b ||a<b)
+{
+printf("%c  it is not an string");
+}
+getch();
+}
+
