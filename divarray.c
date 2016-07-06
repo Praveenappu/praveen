3b3c1a114117dd539d984f79e9929e43faa584fb
Praveen kumar j <pravinair0@gmail.com>
2016-07-06 11:59:41 +0530

    Create divarray.c

---
+++ divarray.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[7],b[5],c;
+printf("Enter the numbers:\n");
+scanf("%d%d",&a[7],&b[7]);
+c=a[7]/b[5];
+printf("divide an array is:%d",c);
+getch();
+}
